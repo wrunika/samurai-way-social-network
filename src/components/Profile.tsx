53bd6d9d08391677c5145474ffbc5909fb9c2c9d
wrunika <2805nika@gmail.com>
2022-12-08 14:12:20 +0300

    refactor: add Profile.module.css

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-
+import styles from './Profile.module.css'
 
 const Profile = () => {
     return(
-        <div className={"content"}>
+        <div className={styles.content}>
             <div>
                 <img src={"https://avatars.mds.yandex.net/i?id=1209512ffa4bff4bd792412b038f0aac2a77ca13-7012254-images-thumbs&n=13"} alt={""}/>
             </div>
             <div>
-                <img src={"https://avatars.mds.yandex.net/i?id=193f144b7eae00375ae3ac129ae1ca7f-5443655-images-thumbs&n=13"} alt={""}/>
+                <img className={styles.avatar} src={"https://avatars.mds.yandex.net/i?id=193f144b7eae00375ae3ac129ae1ca7f-5443655-images-thumbs&n=13"} alt={""}/>
                 ava + description
             </div>
             <div>
