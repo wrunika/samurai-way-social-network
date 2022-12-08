55fa5a0fdb6c08830d2131439c4466041e21d373
wrunika <2805nika@gmail.com>
2022-12-08 13:04:08 +0300

    feat: create components Header, Navbar, Profile

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Header = () => {
+    return(
+        <header className={"header"}>
+            <img
+                src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTN9TaGrF3qmBtBoXN5TaTdijk8dUfq2z7w6a-QjVoEjtxv2f2IcWph0-e7avSfpgTjdg&usqp=CAU"}
+                alt={""}/>
+        </header>
+    );
+}
+
+export default Header;
