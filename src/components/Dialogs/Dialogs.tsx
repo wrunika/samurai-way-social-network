2de1a2653679293da861445057f3547605b7cfdc
wrunika <2805nika@gmail.com>
2022-12-20 23:26:37 +0300

    feat: add some dialogs and basic styles to Dialogs

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -3,8 +3,47 @@ import styles from './Dialogs.module.css'
 
 const Dialogs = () => {
     return(
-        <div>
-            Dialogs
+        <div className={styles.dialogs}>
+            <div className={styles.dialogsItems}>
+                <div className={styles.dialog}>
+                    Maks
+                </div>
+                <div className={styles.dialog}>
+                    Nick
+                </div>
+                <div className={styles.dialog + ' ' + styles.active}>
+                    Helen
+                </div>
+                <div className={styles.dialog}>
+                    Kris
+                </div>
+                <div className={styles.dialog}>
+                    Mike
+                </div>
+                <div className={styles.dialog}>
+                    Greg
+                </div>
+            </div>
+            <div className={styles.messages}>
+                <div className={styles.message}>
+                    Hi.
+                </div>
+                <div className={styles.message}>
+                    How have you been?
+                </div>
+                <div className={styles.message}>
+                    It was a perfect day!
+                </div>
+                <div className={styles.message}>
+                    Hm...
+                </div>
+                <div className={styles.message}>
+                    Nope.
+                </div>
+                <div className={styles.message}>
+                    Excuse me.
+                </div>
+            </div>
         </div>
     )
 }
