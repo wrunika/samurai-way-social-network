64c464dc132d9c5b098898c49fce2421981b129f
wrunika <2805nika@gmail.com>
2022-12-20 17:14:35 +0300

    feat: add BrowserRouter and Route with path and component

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,29 @@ import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
 import Dialogs from "./components/Dialogs/Dialogs";
+import Music from "./components/Music/Music";
+import {BrowserRouter, Route} from "react-router-dom";
+import News from "./components/News/News";
+import Settings from "./components/Settings/Settings";
+
 
 function App() {
     return (
-        <div className="app-wrapper">
-            <Header />
-            <Navbar />
-            <div className="app-wrapper-content">
-                {/*<Profile />*/}
-                <Dialogs />
+        <BrowserRouter>
+            <div className="app-wrapper">
+                <Header/>
+                <Navbar/>
+                <div className="app-wrapper-content">
+                    <Route path="/dialogs" component={Dialogs} />
+                    <Route path="/profile" component={Profile} />
+                    <Route path="/news" component={News} />
+                    <Route path="/music" component={Music} />
+                    <Route path="/settings" component={Settings} />
+                    {/*<Profile />*/}
+                    {/*<Dialogs />*/}
+                </div>
             </div>
-        </div>
+        </BrowserRouter>
     );
 }
 
