fc9a8eef33c05ac8f699a4bc3ece3092481bec66
wrunika <2805nika@gmail.com>
2023-01-24 22:51:11 +0300

    refactor: transfer data from MyPosts to Profile

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -1,20 +1,24 @@
 import React from 'react';
 import Post from "./Post/Post";
 import styles from "./MyPosts.module.css";
+import {PostDataType} from "../Profile";
 
-type PostDataType = {
+/*type PostDataType = {
     id: string
     message: string
     likesCount: number
+}*/
+type MyPostsPropsType = {
+    postsData: PostDataType[]
 }
 
-const MyPosts = () => {
-    let postsData: PostDataType[] = [
+const MyPosts = (props: MyPostsPropsType) => {
+    /*let postsData: PostDataType[] = [
         {id: "1", message: "It is my first post.", likesCount: 5},
         {id: "2", message: "Hi, how have you been?", likesCount: 3},
         {id: "3", message: "The weather is nice today!", likesCount: 1},
-    ]
-    let postsElements = postsData.map(p => <Post message={p.message} likesCount={p.likesCount} />)
+    ]*/
+    let postsElements = props.postsData.map(p => <Post message={p.message} likesCount={p.likesCount} />)
     return (
         <div className={styles.postsBlock}>
             <h3>My posts</h3>
--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -3,12 +3,23 @@ import styles from './Profile.module.css'
 import MyPosts from "./MyPosts/MyPosts";
 import ProfileInfo from "./MyPosts/ProfileInfo/ProfileInfo";
 
-
+export type PostDataType = {
+    id: string
+    message: string
+    likesCount: number
+}
 const Profile = () => {
+
+    let postsData: PostDataType[] = [
+        {id: "1", message: "It is my first post.", likesCount: 5},
+        {id: "2", message: "Hi, how have you been?", likesCount: 3},
+        {id: "3", message: "The weather is nice today!", likesCount: 1},
+    ]
+
     return (
         <div>
             <ProfileInfo />
-            <MyPosts />
+            <MyPosts postsData={postsData} />
         </div>
     );
 }
