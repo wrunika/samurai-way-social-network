36dd33e43fd8db53e41c168f7cc3e4fc786e499c
wrunika <2805nika@gmail.com>
2023-05-01 16:30:07 +0300

    refactor: add function compose into DialogsContainer

--- src/components/Dialogs/DialogsContainer.tsx
+++ src/components/Dialogs/DialogsContainer.tsx
@@ -3,7 +3,7 @@ import {DialogsPageType, sendMessageAC, updateNewMessageBodyAC} from "../../redu
 import Dialogs from "./Dialogs";
 import {connect} from "react-redux";
 import {AppStateType} from "../../redux/redux-store";
-import {Dispatch} from "redux";
+import {compose, Dispatch} from "redux";
 import {withAuthRedirect} from "../../hoc/withAuthRedirect";
 
 
@@ -36,9 +36,10 @@ const mapDispatchToProps = (dispatch: Dispatch): MapDispatchToPropsType => {
     }
 }
 
-export const DialogsContainer = withAuthRedirect(connect(mapStateToProps, mapDispatchToProps) (Dialogs));
-
-
+export const DialogsContainer = compose<React.ComponentType>(
+    connect(mapStateToProps, mapDispatchToProps),
+    withAuthRedirect
+) (Dialogs)
 
 
 
