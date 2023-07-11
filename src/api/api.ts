91bd66bbc93df8c23f847f560eedb1fe6db16041
wrunika <2805nika@gmail.com>
2023-07-11 23:41:44 +0300

    feat: add securityAPI for captcha into api.ts

--- src/api/api.ts
+++ src/api/api.ts
@@ -56,12 +56,19 @@ export const authAPI = {
         return instance.get(`auth/me`)
             .then(response => response.data)
     },
-    login(email: string, password: string, rememberMe: boolean = false) {
-        return instance.post(`auth/login`, {email, password, rememberMe})
+    login(email: string, password: string, rememberMe: boolean = false, captchaUrl: string | null = null) {
+        return instance.post(`auth/login`, {email, password, rememberMe, captchaUrl})
             .then(response => response.data)
     },
     logout() {
         return instance.delete(`auth/login`)
+            .then(response => response.data)
+    }
+}
+
+export const securityAPI = {
+    getCaptchaUrl() {
+        return instance.get(`security/get-captcha-url`)
             .then(response => response.data)
     }
 }
