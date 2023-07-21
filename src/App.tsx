de8ce1c45b1bc635dad420b1793f9367bc5ae15c
wrunika <2805nika@gmail.com>
2023-07-21 20:56:35 +0300

    fix: fix Switch into App

--- src/App.tsx
+++ src/App.tsx
@@ -43,19 +43,21 @@ class App extends React.Component<AppPropsType, AuthType> {
             <div className="app-wrapper">
                 <HeaderContainer/>
                 <Navbar/>
-                <Switch>
-                <div className="app-wrapper-content">
-                    <Route exact path={"/"} render={() => <Redirect to={"/profile"} />} />
-                    <Route path="/dialogs" render={withSuspense(DialogsContainer)}/>
-                    <Route path="/profile/:userId?" render={withSuspense(ProfileContainer)}/>
-                    <Route path="/users" render={() => <UsersContainer/>}/>
-                    <Route path="/news" component={News}/>
-                    <Route path="/music" component={Music}/>
-                    <Route path="/settings" component={Settings}/>
-                    <Route path="/login" render={withSuspense(Login)}/>
-                    <Route path="*" render={() => <div>404 NOT FOUND</div>}/>
-                </div>
-                </Switch>
+
+                    <div className="app-wrapper-content">
+                        <Switch>
+                        <Route exact path={"/"} render={() => <Redirect to={"/profile"} />} />
+                        <Route path="/dialogs" render={withSuspense(DialogsContainer)}/>
+                        <Route path="/profile/:userId?" render={withSuspense(ProfileContainer)}/>
+                        <Route path="/users" render={() => <UsersContainer/>}/>
+                        <Route path="/news" component={News}/>
+                        <Route path="/music" component={Music}/>
+                        <Route path="/settings" component={Settings}/>
+                        <Route path="/login" render={withSuspense(Login)}/>
+                        <Route path="*" render={() => <div>404 NOT FOUND</div>}/>
+                        </Switch>
+                    </div>
+
             </div>
         );
     }
